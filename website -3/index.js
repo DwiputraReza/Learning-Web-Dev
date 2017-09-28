54e50dcea8a9171abedcccf257589b3155b9f7ec
DwiputraReza <dwiputra.reza.88@gmail.com>
2017-09-28 15:36:08 +0700

    belajar web dev

---
+++ website -3/index.js
@@ -0,0 +1,16 @@
+var nama = prompt ("siapa nama anda");
+var alamat = prompt (" dimana anda tinggal");
+var umur= prompt ("berapa usia anda");
+var umurDalamHari= umur*365
+
+alert ("Anda bernama " + nama + " tinggal di " + alamat + " berumur " + umur +" tahun ");
+
+alert ( "Umur anda apabila di jadikan hari adalah = "+ umurDalamHari + "hari")
+
+if(parseInt.umur>17) {
+    alert("Anda sudah cukup umur, Silahkan Masuk")
+    }
+    else {
+     alert("Anda belum cukup umur");
+    }
+    
